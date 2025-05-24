ea760ef11eded4c34cf8eee1452ef029dcc39bff
Navya <navya.bhandari@outlook.com>
2025-05-23 20:13:09 -0700

    debug test api thing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\File;
 Route::get('/', function () {
     return File::get(public_path('chatbotTest.html'));
 });
+
+Route::get('/test-api', function () {
+    return response()->json(['message' => 'API route is working!']);
+});
