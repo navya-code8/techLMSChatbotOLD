2efd146852dc9411b962c8dae7b097be8e499ed9
Navya <navya.bhandari@outlook.com>
2025-05-23 20:17:53 -0700

    some more api debugging

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\ChatbotController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/ask', [ChatbotController::class, 'ask']);
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API GET test route works']);
+});
